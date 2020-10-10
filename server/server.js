8e9ee26d3298cdd982a824154865a868143e547d
Sylvia <sylviaprabudy@gmail.com>
2020-10-09 23:27:08 -0500

    install and add React

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const express = require('express');
-const path = require('path');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -19,6 +20,16 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
